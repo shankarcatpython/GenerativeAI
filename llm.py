6b1f8698b4505e4c8f246f2295bf7ff13abf6bc7
New User <shankarcat@yahoo.com>
2024-05-30 15:50:58 -0400

    Updated with correct Chat completion

--- llm.py
+++ llm.py
@@ -3,27 +3,24 @@ import config
 
 
 def askme_questions_summarize(prompt):
-    final_prompt = "Summarize the following paragraph" + prompt
+ 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are very good on summarizing"},
-            {"role": "user", "content": final_prompt}
+            {"role": "system", "content": "You are a helpful elucidator"},
+            {"role": "user", "content": f"Summarize information about {prompt}"}
         ]
     )
 
     return response.choices[0].message['content']
 
 def askme_questions_suggestion(prompt):
-    final_prompt_sugg = "Suggest some thing from following paragraph" + prompt
     response_sugg = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are very good at suggesting things"},
-            {"role": "user", "content": final_prompt_sugg}
+            {"role": "system", "content": "You are a helpful counsellor"},
+            {"role": "user", "content": f"Suggest about {prompt}"}
         ]
     )
 
     return response_sugg.choices[0].message['content']
-
-
